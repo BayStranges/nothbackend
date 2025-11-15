6da70bd1fe55b3497df548924247a0b02e5f8a7e
BayStranges <emircantari01@gmail.com>
2025-11-15 23:12:03 +0300

    sa

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const app = express();
 const port = 3000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '../frontend')));
 
 // Sample menu data in multiple languages
 const menuData = {
